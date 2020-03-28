b332b1c06958f763c521c92ed71d9c9d22f0fed0
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-28 15:57:42 +0000

    st.sh

---
+++ st.sh
@@ -0,0 +1,17 @@
+# 28-3-2020 JHZ
+
+git add .gitignore
+git commit -m ".gitignore"
+git add README.md
+git commit -m "README"
+git add doc
+git commit -m "Documents and auxiliary files"
+git add -f cvd2/README.txt cvd2/*sh
+git commit -m "CVD2"
+git add -f ACE2/2805-6_2.metal ACE2/select.sh
+git commit -m "SomaLogic"
+git add -f GEO/test.R
+git commit -m "GEO test"
+git add st.sh
+git commit -m "st.sh"
+git push
