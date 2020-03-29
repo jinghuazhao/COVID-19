a1478158c346d46a5acf048cb9e88b26990e4857
jinghuazhao <jinghuazhao@hotmail.com>
2020-03-29 22:34:12 +0100

    GEO test

--- GEO/test.R
+++ GEO/test.R
@@ -9,5 +9,5 @@ for (src in c("Cryobiopsy","Donor","IPF","Myositis"))
 {
   lst <- scan(paste0(src,".list"),"")
   h5mm(lst)
-  for(i in 1:length(lst) investigate(lst[i])
+  for(i in 1:length(lst)) investigate(lst[i])
 }
