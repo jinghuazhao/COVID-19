1acd6fd861869b75e1739f15ca77bc7528f32059
jinghuazhao <jinghuazhao@hotmail.com>
2020-04-05 17:54:53 +0100

    GEO test

---
+++ GEO/GEO2R.R
@@ -0,0 +1,18 @@
+# 5-4-2020 JHZ
+
+library(Biobase)
+library(GEOquery)
+
+# load series and platform data from GEO
+
+gset <- getGEO("GSE4115", GSEMatrix =TRUE, getGPL=FALSE)
+if (length(gset) > 1) idx <- grep("GPL96", attr(gset, "names")) else idx <- 1
+gset <- gset[[idx]]
+
+# set parameters and draw the plot
+
+dev.new(width=4+dim(gset)[[2]]/5, height=6)
+par(mar=c(2+round(max(nchar(sampleNames(gset)))/2),4,2,1))
+title <- paste ("GSE4115", '/', annotation(gset), " selected samples", sep ='')
+boxplot(exprs(gset), boxwex=0.7, notch=T, main=title, outline=FALSE, las=2)
+
