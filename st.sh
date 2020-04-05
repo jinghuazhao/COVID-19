a348671ae14e53d0f7212650dda3bf6696001612
jinghuazhao <jinghuazhao@hotmail.com>
2020-04-05 17:30:18 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,4 @@
-# 29-3-2020 JHZ
+# 5-4-2020 JHZ
 
 git add .gitignore
 git commit -m ".gitignore"
@@ -10,7 +10,7 @@ git add -f cvd2/README.txt cvd2/*sh
 git commit -m "CVD2"
 git add -f ACE2/2805-6_2.metal ACE2/select.sh
 git commit -m "SomaLogic"
-git add -f GEO/test.ini GEO/test.R GEO/test.sh GEO/README.md
+git add -f GEO/GDS.R GEO/test.ini GEO/test.R GEO/test.sh GEO/README.md
 git commit -m "GEO test"
 git add st.sh
 git commit -m "st.sh"
