a0314f6115b8d0dae6ee165743a5d43e49548a5e
jinghuazhao <jinghuazhao@hotmail.com>
2020-04-18 22:06:06 +0100

    SomaLogic

--- ACE2/ukb-ACE2-annotate.sh
+++ ACE2/ukb-ACE2-annotate.sh
@@ -1,4 +1,4 @@
-# 17-4-2020 JHZ
+# 18-4-2020 JHZ
 
 export UKB=/rds/project/jmmh2/rds-jmmh2-post_qc_data/uk_biobank/imputed/uk10k_hrc/HRC_UK10K
 export HPC_WORK=/rds/user/${USER}/hpc-work
@@ -15,7 +15,7 @@ R --no-save -q <<END
   snpstats <- read.table("ukb-ACE2.snpstats",as.is=TRUE,header=TRUE)
   ord <- with(snpstats,order(chromosome,position))
   ct <- snpstats[ord,]
-  all <- within(ct,{chr <- chromosome; pos <- position; snp <- rsid; a1 <- A; a2 <- B; qual <- "."; filter <- ".";})
+  all <- within(ct,{chr <- chromosome; pos <- position; snp <- rsid; a1 <- alleleA; a2 <- alleleB; qual <- "."; filter <- ".";})
   writetable <- function(d,f,...) write.table(d,file=f,col.names=FALSE,row.names=FALSE,quote=FALSE,sep="\t",...)
   for (f in c("ukb-ACE2"))
   {
@@ -30,9 +30,10 @@ R --no-save -q <<END
   }
 END
 
-export wd=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/ACE2
 ln -sf /rds/user/jhz22/hpc-work/ensembl-vep/clinvar_GRCh37.vcf.gz
 ln -sf /rds/user/jhz22/hpc-work/ensembl-vep/clinvar_GRCh37.vcf.gz.tbl
+
+export wd=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/ACE2
 
 for s in ukb-ACE2
 do
@@ -44,9 +45,7 @@ do
         -operation g,g,g,g,r,f,f,f,r \
         -remove -nastring . -csvout -polish -xref $ANNOVA/example/gene_xref.txt
    # Polyphen-2
-   cut -d' ' -f8 ${s}.ld | \
-   sed '1d;s/_/ /;s/_/\//' | \
-   sort -k1,1 | \
+   awk 'NR>1{print "chr" $3 ":" $4, $5 "/" $6}' ${s}.snpstats | \
    uniq > ${s}.pph.list
    mapsnps.pl -g hg19 -m -U -y ${s}.pph.input ${s}.pph.list 1>${s}.pph.features 2>${s}.log
    run_pph.pl ${s}.pph.input 1>${s}.pph.output 2>${s}.pph.log
@@ -62,6 +61,14 @@ do
        --plugin LoF,loftee_path:.,human_ancestor_fa:human_ancestor.fa.gz \
        --plugin dbNSFP,${VEP}/dbNSFP4.0a/dbNSFP4.0a.gz,${dbNSFP_fields}
    cd -
+## Where the selected ClinVar INFO fields (from the ClinVar VCF file) are:
+# - CLNSIG:     Clinical significance for this single variant
+# - CLNREVSTAT: ClinVar review status for the Variation ID
+# - CLNDN:      ClinVar's preferred disease name for the concept specified by disease identifiers in CLNDISDB
+# Or the INFO fields you want in the ClinVar VCF file
+  vep --i ukb-ACE2.vepinput --species homo_sapiens -o ukb-ACE2.clinvar --cache --offline --force_overwrite \
+      --custom clinvar_GRCh37.vcf.gz,ClinVar,vcf,exact,0,CLNSIG,CLNREVSTAT,CLNDN --tab \
+      --fields Uploaded_variation,Gene,Consequence,ClinVar_CLNSIG,ClinVar_CLNREVSTAT,ClinVar_CLNDN
 done
 
 function IPD()
@@ -69,13 +76,4 @@ function IPD()
   bgenix -g ${UKB}/ukb_imp_chrX_v3.bgen -list -incl-range X:15579156-15620192 > ukb-ACE2
   bgenix -g ${UKB}/ukb_imp_chrX_v3.bgen -vcf -incl-range X:15579156-15620192  > ukb-ACE2.vcf
 
-## Where the selected ClinVar INFO fields (from the ClinVar VCF file) are:
-# - CLNSIG:     Clinical significance for this single variant
-# - CLNREVSTAT: ClinVar review status for the Variation ID
-# - CLNDN:      ClinVar's preferred disease name for the concept specified by disease identifiers in CLNDISDB
-# Or the INFO fields you want in the ClinVar VCF file
-
-  vep --i ukb-ACE2.vcf --species homo_sapiens -o ukb-ACE2.vepout --cache --offline --force_overwrite \
-      --custom clinvar_GRCh37.vcf.gz,ClinVar,vcf,exact,0,CLNSIG,CLNREVSTAT,CLNDN --tab \
-      --fields Uploaded_variation,Gene,Consequence,ClinVar_CLNSIG,ClinVar_CLNREVSTAT,ClinVar_CLNDN
 }
