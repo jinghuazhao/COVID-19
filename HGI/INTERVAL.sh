3d9986b0612af2fd83108bf6d529a9363cb2faf8
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-12 17:08:52 +0100

    INTERVAL analysis

--- HGI/INTERVAL.sh
+++ HGI/INTERVAL.sh
@@ -1,6 +1,11 @@
-// 12-5-2020 JHZ
+# 12-5-2020 JHZ
 
-// sed '1d' INTERVAL_Covid_06MAY2020.csv | cut -d',' -f1| sort | join - <(sed '1d' INTERVALdata_06MAY2020.csv | cut -d',' -f1) | wc -l
-// 400
-// /home/jhz22/rds/post_qc_data/interval/reference_files/genetic/interval
-//
+cd 06-05-2020/INTERVAL
+sed '1d' INTERVAL_Covid_06MAY2020.csv | cut -d',' -f1 | sort | join - <(sed '1d' INTERVALdata_06MAY2020.csv | cut -d',' -f1) | wc -l
+sed '1d' INTERVAL_Covid_06MAY2020.csv | cut -d',' -f1 | sort | join - <(sed '1d' INTERVAL_OmicsMap_20200506.csv | cut -d',' -f1) | wc -l
+sed '1d' INTERVAL_Covid_06MAY2020.csv | cut -d',' -f1 | sort | join - <(sed '1d' INTERVAL_OmicsMap_p3_20200506.csv | cut -d',' -f1) | wc -l
+sed '1d' INTERVALdata_P3_06MAY2020.csv | cut -d',' -f1 | sort | join - <(sed '1d' INTERVAL_OmicsMap_p3_20200506.csv | cut -d',' -f1) | wc -l
+cd -
+export REF=/home/jhz22/rds/post_qc_data/interval/reference_files/genetic/interval
+export EV=$REF/annot_INT_50PCs_pcs.txt
+export snpstats=$REF/impute_${chr}_interval.snpstats
