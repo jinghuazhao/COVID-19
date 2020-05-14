101bd24f03395f2730d1b69306475e8308285776
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-14 18:10:31 +0100

    INTERVAL analysis

--- HGI/INTERVAL.do
+++ HGI/INTERVAL.do
@@ -1,11 +1,11 @@
-// 12-5-2020 JHZ
+// 13-5-2020 JHZ
 
 local dir : env dir
 local ev : env ev
 insheet using "`ev'", case clear delim(" ")
 keep ID Batch PC_1-PC_20
 sort ID
-rename ID Affymetrix_gwasQC_bl
+format ID %15.0g
 save INTERVAL-pca, replace
 insheet using "`dir'/06-05-2020/INTERVAL/INTERVAL_Covid_06MAY2020.csv", case clear
 sort identifier
@@ -17,12 +17,18 @@ insheet using "`dir'/06-05-2020/INTERVAL/INTERVAL_OmicsMap_20200506.csv", case c
 sort identifier
 merge 1:1 identifier using INTERVAL, gen(dataid)
 keep if data==3
-duplicates tag Affymetrix_gwasQC_bl, gen(dup)
+rename Affymetrix_gwasQC_bl ID
+format ID %15.0g
+duplicates tag ID, gen(dup)
 keep if dup==0
 drop dup
-merge 1:1 Affymetrix_gwasQC_bl using INTERVAL-pca, gen(dataidpca)
+merge 1:1 ID using INTERVAL-pca, gen(dataidpca)
 keep if dataidpca==3
 merge 1:1 identifier using covid, gen(intervalcovid)
 keep if intervalcovid==3
-outsheet Affymetrix_gwasQC_bl agePulse sexPulse PC_1-PC_20 using INTERVAL-covid.txt, noquote replace
+rename agePulse age
+rename sexPulse sex
+egen SARS_CoV=rowtotal(SARS_CoV2_1 SARS_CoV2_2 SARS_CoV2_3 SARS_CoV2_4 SARS_CoV2_5 SARS_CoV2_6)
+replace SARS_CoV=1 if SARS_CoV>0
+outsheet ID SARS_CoV age sex PC_1-PC_20 using INTERVAL-covid.txt, delim(" ") noquote replace
 gzsave INTERVAL-covid, replace
--- HGI/INTERVAL.sh
+++ HGI/INTERVAL.sh
@@ -1,4 +1,119 @@
-# 12-5-2020 JHZ
+# 14-5-2020 JHZ
+
+export dir=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/HGI
+export autosomes=/home/jhz22/rds/post_qc_data/interval/imputed/uk10k_1000g_b37/imputed
+export X=/rds/project/jmmh2/rds-jmmh2-projects/covid/ace2/interval_genetic_data/interval_imputed_data
+export merged_imputation=/home/jhz22/rds/post_qc_data/interval/genotype/affy_ukbiobank_array/genotyped/merged_imputation
+export ref=/home/jhz22/rds/post_qc_data/interval/reference_files/genetic/interval
+export ev=$ref/annot_INT_50PCs_pcs.txt
+
+function phenofile()
+{
+  module load ceuadmin/stata
+  stata -b do INTERVAL.do
+}
+
+function genofile()
+{
+# SBATCH -A CARDIO-SL0-CPU -p cardio_intr --qos=cardio_intr
+  module load plink/2.00-alpha
+  plink2 --bfile ${merged_imputation} --indep-pairwise 1000kb 1 0.1 --out INTERVAL
+  plink2 --bfile ${merged_imputation} --make-bed --extract INTERVAL.prune.in --out INTERVAL
+  sed '1d' INTERVAL-covid.txt | \
+  cut -d' ' -f1 > INTERVAL.samples
+  seq 22 | \
+  parallel --env autosomes -C' ' '
+    qctool -g ${autosomes}/impute_{}_interval.bgen -s ${autosomes}/interval.samples -incl-samples INTERVAL.samples -og INTERVAL-{}.bgen
+    bgenix -g INTERVAL-{}.bgen -index -clobber
+  '
+  awk '{print $1 "_" $1}' INTERVAL.samples | \
+  bcftools view -S - ${X}/INTERVAL_X_imp_ann_filt_v2.vcf.gz -O v --force-samples > X.vcf
+  cat INTERVAL.samples | \
+  parallel --dry-run -C' ' "
+    export s={}_{};
+    export t={};
+    sed -i 's/'\"\${s}\"'/'\"\${t}\"'/g' X.vcf
+  "
+  ) | bash
+  qctool -g X.vcf -og X.bgen
+  bgenix -g X.bgen -index -clobber
+}
+
+# Analysis 5-susceptibility (phenotype name: ANA5):
+
+# Single-variant association tests
+
+step1_fitNULLGLMM.R \
+   --plinkFile=INTERVAL \
+   --phenoFile=INTERVAL-covid.txt \
+   --phenoCol=SARS_CoV \
+   --covarColList=age,sex,PC_1,PC_2,PC_3,PC_4,PC_5,PC_6,PC_7,PC_8,PC_9,PC_10,PC_11,PC_12,PC_13,PC_14,PC_15,PC_16,PC_17,PC_18,PC_19,PC_20 \
+   --sampleIDColinphenoFile=ID \
+   --traitType=binary \
+   --outputPrefix=INTERVAL \
+   --nThreads=4
+
+echo $(seq 22) X | \
+parallel --env autosomes -C' ' '
+step2_SPAtests.R \
+   --bgenFile=INTERVAL-{}.bgen \
+   --bgenFileIndex=INTERVAL-{}.bgen.bgi \
+   --minMAF=0.0001 \
+   --minMAC=1 \
+   --sampleFile=INTERVAL-covid.txt \
+   --GMMATmodelFile=INTERVAL.rda \
+   --varianceRatioFile=INTERVAL.varianceRatio.txt \
+   --SAIGEOutputFile=INTERVAL-{}.txt \
+   --numLinesOutput=2 \
+   --IsOutputNinCaseCtrl=TRUE \
+   --IsOutputHetHomCountsinCaseCtrl=TRUE \
+   --IsOutputAFinCaseCtrl=TRUE
+'
+
+# Gene-based association tests
+
+createSparseGRM.R \
+   --plinkFile=INTERVAL \
+   --minMAF=0.0001 \
+   --minMAC=1 \
+   --sampleFile=INTERVAL-covid.txt \
+   --nThreads=4 \
+   --outputPrefix=INTERVAL.sparseGRM \
+   --numRandomMarkerforSparseKin=2000 \
+   --relatednessCutoff=0.125
+
+echo $(seq 22) X | \
+parallel --env autosomes -C' ' '
+step2_SPAtests.R \
+   --bgenFile=INTERVAL-{}.bgen \
+   --bgenFileIndex=INTERVAL-{}.bgen.bgi \
+   --chrom={} \
+   --minMAF=0 \
+   --minMAC=0.5 \
+   --maxMAFforGroupTest=0.01 \
+   --sampleFile=INTERVAL-covid.txt \
+   --GMMATmodelFile=INTERVAL.rda \
+   --varianceRatioFile=INTERVAL.varianceRatio.txt \
+   --SAIGEOutputFile=INTERVAL.SAIGE.gene-{}.txt \
+   --numLinesOutput=1 \
+   --groupFile=INTERVAL.eneBasedtest.txt \
+   --sparseSigmaFile=INTERVAL.sparseSigma.mtx \
+   --IsOutputAFinCaseCtrl=TRUE \
+   --IsSingleVarinGroupTest=TRUE \
+   --IsOutputPvalueNAinGroupTestforBinary=TRUE \
+   --IsAccountforCasecontrolImbalanceinGroupTest=TRUE
+'
+##
+
+# createSparseGRM.R --help
+# step1_fitNULLGLMM.R --help
+# step2_SPAtests.R --help
+
+(
+  cut -f1,7,8,15,18,19 $ref/impute_*_interval.snpstats | \
+  head -1
+  parallel --env ref -C' ' 'sed '1d' $ref/impute_${chr}_interval.snpstats | cut -f1,7,8,15,18,19 $ref/impute_1_interval.snpstats'
+) | gzip -f INTERVAL.snpstats.gz
 
 cd 06-05-2020/INTERVAL
 sed '1d' INTERVAL_Covid_06MAY2020.csv | cut -d',' -f1 | sort | join - <(sed '1d' INTERVALdata_06MAY2020.csv | cut -d',' -f1) | wc -l
@@ -6,74 +121,3 @@ sed '1d' INTERVAL_Covid_06MAY2020.csv | cut -d',' -f1 | sort | join - <(sed '1d'
 sed '1d' INTERVAL_Covid_06MAY2020.csv | cut -d',' -f1 | sort | join - <(sed '1d' INTERVAL_OmicsMap_p3_20200506.csv | cut -d',' -f1) | wc -l
 sed '1d' INTERVALdata_P3_06MAY2020.csv | cut -d',' -f1 | sort | join - <(sed '1d' INTERVAL_OmicsMap_p3_20200506.csv | cut -d',' -f1) | wc -l
 cd -
-export dir=/rds/project/jmmh2/rds-jmmh2-projects/olink_proteomics/scallop/HGI
-export autosomes=/home/jhz22/rds/post_qc_data/interval/imputed/uk10k_1000g_b37/imputed
-export X=/rds/project/jmmh2/rds-jmmh2-projects/covid/ace2/interval_genetic_data/interval_imputed_data
-export ref=/home/jhz22/rds/post_qc_data/interval/reference_files/genetic/interval
-export ev=$ref/annot_INT_50PCs_pcs.txt
-export snpstats=$ref/impute_${chr}_interval.snpstats
-
-module load ceuadmin/stata
-stata -b do INTERVAL.do
-
-# ${autosomes}/impute_${chr}_interval.bgen.bgi
-# ${X}/INTERVAL_X_imp_ann_filt_v2.vcf.gz
-
-# Rscript createSparseGRM.R --help
-# Rscript step1_fitNULLGLMM.R --help
-# Rscript step2_SPAtests.R --help
-
-# single-variant association tests
-
-Rscript step1_fitNULLGLMM.R \
-   --plinkFile=./input/nfam_100_nindep_0_step1_includeMoreRareVariants_poly \
-   --phenoFile=./input/pheno_1000samples.txt_withdosages_withBothTraitTypes.txt \
-   --phenoCol=y_binary \
-   --covarColList=x1,x2 \
-   --sampleIDColinphenoFile=IID \
-   --traitType=binary \
-   --outputPrefix=./output/example_binary \
-   --nThreads=4
-
-Rscript step2_SPAtests.R \
-   --bgenFile=./input/genotype_100markers.bgen \
-   --bgenFileIndex=./input/genotype_100markers.bgen.bgi \
-   --minMAF=0.0001 \
-   --minMAC=1 \
-   --sampleFile=./input/samplefileforbgen_10000samples.txt \
-   --GMMATmodelFile=./output/example.rda \
-   --varianceRatioFile=./output/example.varianceRatio.txt \
-   --SAIGEOutputFile=./output/example.SAIGE.bgen.txt \
-   --numLinesOutput=2 \
-   --IsOutputNinCaseCtrl=TRUE \
-   --IsOutputHetHomCountsinCaseCtrl=TRUE \
-   --IsOutputAFinCaseCtrl=TRUE
-
-# Gene-based association tests
-
-createSparseGRM.R \
-   --plinkFile=./input/nfam_100_nindep_0_step1_includeMoreRareVariants_poly \
-   --nThreads=4 \
-   --outputPrefix=./output/sparseGRM \
-   --numRandomMarkerforSparseKin=2000 \
-   --relatednessCutoff=0.125
-
-Rscript step2_SPAtests.R \
-   --vcfFile=./input/seedNumLow_126001_seedNumHigh_127000_nfam_1000_nindep_0.sav \
-   --vcfFileIndex=./input/seedNumLow_126001_seedNumHigh_127000_nfam_1000_nindep_0.sav.s1r \
-   --vcfField=DS \
-   --chrom=chr1 \
-   --minMAF=0 \
-   --minMAC=0.5 \
-   --maxMAFforGroupTest=0.01       \
-   --sampleFile=./input/samplelist.txt \
-   --GMMATmodelFile=./output/example_binary.rda \
-   --varianceRatioFile=./output/example_binary_cate_v2.varianceRatio.txt \
-   --SAIGEOutputFile=./output/example_binary.SAIGE.gene.txt \
-   --numLinesOutput=1 \
-   --groupFile=./input/groupFile_geneBasedtest.txt \
-   --sparseSigmaFile=./output/example_binary_cate_v2.varianceRatio.txt_relatednessCutoff_0.125.sparseSigma.mtx \
-   --IsOutputAFinCaseCtrl=TRUE \
-   --IsSingleVarinGroupTest=TRUE \
-   --IsOutputPvalueNAinGroupTestforBinary=TRUE \
-   --IsAccountforCasecontrolImbalanceinGroupTest=TRUE
