1a24c807a8792748717165f5c6fbf8f276e51e69
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-15 21:37:32 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -1,4 +1,4 @@
-# 12-5-2020 JHZ
+# 15-5-2020 JHZ
 
 git add .gitignore
 git commit -m ".gitignore"
@@ -12,7 +12,7 @@ git add -f ACE2/2805-6_2.metal ACE2/select.sh ACE2/ukb-ACE2-annotate.sh ACE2/ace
 git commit -m "SomaLogic"
 git add -f GEO/GEO.R GEO/GEO2R.R GEO/test.ini GEO/test.R GEO/test.sh GEO/README.md
 git commit -m "GEO test"
-git add -f HGI/INTERVAL.do HGI/INTERVAL.sh
+git add -f HGI/INTERVAL.do HGI/INTERVAL.sh HGI/glist-hg19.sh
 git commit -m "INTERVAL analysis"
 git add st.sh
 git commit -m "st.sh"
