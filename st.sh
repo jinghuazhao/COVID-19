67372df7625df6125607b0cc7c5bbd0027bf041f
jinghuazhao <jinghuazhao@hotmail.com>
2020-05-28 11:12:32 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -6,13 +6,13 @@ git add README.md
 git commit -m "README"
 git add doc
 git commit -m "Documents and auxiliary files"
-git add -f cvd2/README.txt cvd2/*sh cvd2/ps.R
+git add cvd2/README.txt cvd2/*sh cvd2/ps.R
 git commit -m "CVD2"
-git add -f ACE2/2805-6_2.metal ACE2/select.sh ACE2/ukb-ACE2-annotate.sh ACE2/ace2_interval_imputed_ukb_imputed.sh
+git add ACE2/2805-6_2.metal ACE2/select.sh ACE2/ukb-ACE2-annotate.sh ACE2/ace2_interval_imputed_ukb_imputed.sh
 git commit -m "SomaLogic"
-git add -f GEO/GEO.R GEO/GEO2R.R GEO/test.ini GEO/test.R GEO/test.sh GEO/README.md
+git add GEO/GEO.R GEO/GEO2R.R GEO/test.ini GEO/test.R GEO/test.sh GEO/README.md
 git commit -m "GEO test"
-git add -f HGI/INTERVAL.do HGI/INTERVAL.inc HGI/INTERVAL.rec HGI/INTERVAL.sb HGI/INTERVAL.sh HGI/INTERVAL-X.sh HGI/glist-hg19.sh
+git add HGI/INTERVAL.do HGI/INTERVAL.inc HGI/INTERVAL.rec HGI/INTERVAL.sb HGI/INTERVAL.sh HGI/INTERVAL-X.sh HGI/glist-hg19.sh
 git commit -m "HGI analysis"
 git add st.sh
 git commit -m "st.sh"
