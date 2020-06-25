9e0a0cfd037854cdea29edf351ac1e991ac4b632
jinghuazhao <jinghuazhao@hotmail.com>
2020-06-25 12:14:28 +0100

    Olink/NGS pilot

--- NGS/ngs.sh
+++ NGS/ngs.sh
@@ -11,6 +11,7 @@ function ngs()
   insheet using INTERVALdata_19JUN2020.csv, case clear
   save work/data, replace
   insheet using INTERVAL_OmicsMap_20200619.csv, case clear
+  format Affymetrix_gwasQC_bl %15.0g
   format Olink_* %15.0g
   save work/omics, replace
   insheet using Olink_NGS.csv, case clear
@@ -23,10 +24,10 @@ function ngs()
   merge m:1 SampleID using work/dataomics, gen(olink_ngs)
   save work/`panel', replace
   d
-  keep SampleID UniProt LOD NPX
-  sort SampleID UniProt
-  by SampleID: gen j=_n
-  reshape wide UniProt LOD NPX, i(SampleID) j(j)
+  keep Affymetrix_gwasQC_bl UniProt LOD NPX
+  sort Affymetrix_gwasQC_bl UniProt
+  by Affymetrix_gwasQC_bl: gen j=_n
+  reshape wide UniProt LOD NPX, i(Affymetrix_gwasQC_bl) j(j)
   outsheet using work/`panel', noquote replace
   END
 }
@@ -36,14 +37,31 @@ function ngs()
 function corr()
 {
   R --no-save -q <<\ \ END
-# old Olink panels
-  panels <- c("NEUROLOGY", "CARDIOMETABOLIC", "CARDIOMETABOLIC", "INFLAMMATION")
-  qc <- c("neu_qc", "qc_cvd2", "qc_cvd3", "qc_inf")
-  panel <- Sys.getenv("panel")
-  sel <- which(panels==panel)
-  od <- read.csv(paste0("olink_proteomics/qc/olink_",qc[sel],".csv"),as.is=TRUE)
-  names(od) <- toupper(names(od))
+  corr <- function()
+  {
+    od <- read.csv(paste0("olink_proteomics/qc/olink_",qc[sel],".csv"),as.is=TRUE)
+    names(od) <- toupper(names(od))
+    Olink_id <- paste0("Olink_",ids[sel],"_QC_24m")
+    omics <- read.csv("INTERVAL_OmicsMap_20200619.csv",as.is=TRUE)
+    omics_id <- subset(omics,!is.na(omics[Olink_id]))[c(Olink_id,"Affymetrix_gwasQC_bl")]
+    od <- merge(omics_id,od,by.x=Olink_id,by.y="ALIQUOT_ID")
+    odd <- merge(d,od,by="Affymetrix_gwasQC_bl")
+  # correlation
+    for(i in setdiff(intersect(names(d),names(od)),"Affymetrix_gwasQC_bl"))
+    {
+      with(odd, {
+      x <- odd[[paste0(i,".x")]]
+      y <- odd[[paste0(i,".y")]]
+      cat(panel, "-", i, ":", cor(x,y,use="everything"), "\n")
+      })
+    }
+  }
 # new data from NGS
+  panels <- c("NEUROLOGY", "CARDIOMETABOLIC", "INFLAMMATION")
+  qc <- c("neu_qc", "qc_cvd2", "qc_cvd3", "qc_inf")
+  ids <- c("neu","cvd2","cvd3","inf")
+  panel <- Sys.getenv("panel")
+  sel <- which(panel==panels)
   d <-read.delim(paste0("work/",panel,".out"),as.is=TRUE)
   ncols <- length(d[1,-1])/3
   enum <- (1:ncols)*3-1
@@ -52,13 +70,10 @@ function corr()
   names(d)[enum+2] <- uniprot
   d <- d[,-enum]
   write.table(d,file=paste0("work/",panel,".csv"),quote=FALSE,row.names=FALSE,sep=",")
-# correlation
-  for(i in intersect(names(d),names(od)))
-  {
-    x <- d[i]
-    y <- od[i]
-    cor(x,y,use="everything")
-  }
+# old Olink panels
+  cat("Panel =",panel,sel,"\n")
+  corr()
+  if (sel==2) {sel=3; corr()}
   END
 }
 
@@ -66,5 +81,5 @@ for panel in CARDIOMETABOLIC INFLAMMATION NEUROLOGY ONCOLOGY
 do
   export panel=${panel}
 # ngs
-  corr
+  if [ "${panel}" != "ONCOLOGY" ]; then corr; fi
 done
