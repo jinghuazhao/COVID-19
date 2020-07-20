6a18234d56c4879457724b2c359125f12f7731d6
jinghuazhao <jinghuazhao@hotmail.com>
2020-07-20 21:12:02 +0100

    st.sh

--- st.sh
+++ st.sh
@@ -12,7 +12,7 @@ git add ACE2/2805-6_2.metal ACE2/select.sh ACE2/ukb-ACE2-annotate.sh ACE2/ace2_i
 git commit -m "SomaLogic"
 git add GEO/GEO.R GEO/GEO2R.R GEO/test.ini GEO/test.R GEO/test.sh GEO/README.md
 git commit -m "GEO test"
-git add HGI/INTERVAL.do HGI/INTERVAL.inc HGI/INTERVAL.rec HGI/INTERVAL.sh HGI/INTERVAL-X.sh HGI/glist-hg19.sb HGI/glist-hg19.sh HGI/MR.sh
+git add HGI/INTERVAL.do HGI/INTERVAL.inc HGI/INTERVAL.rec HGI/INTERVAL.sh HGI/INTERVAL-X.sh HGI/glist-hg19.sb HGI/glist-hg19.sh HGI/select.sh
 git commit -m "HGI analysis"
 git add NGS/README.md NGS/ngs.do NGS/ngs.R NGS/ngs.sb NGS/ngs.sh NGS/lod.sh NGS/utils
 git commit -m "Olink/NGS pilot"
