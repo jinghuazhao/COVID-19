d44d01c35e9e16c3ce187c1b02d91f56964ad9fa
jinghuazhao <jinghuazhao@hotmail.com>
2020-07-29 15:01:26 +0100

    Olink/NGS pilot

--- NGS/pQTLtools.R
+++ NGS/pQTLtools.R
@@ -1,5 +1,3 @@
-library(gap)
-
 bioc <- function()
 {
   library(GenomicRanges)
@@ -24,8 +22,37 @@ bioc <- function()
   cvt
 }
 
+library(gap)
 library(pQTLtools)
+library(iBMQ)
+options(width=220)
+
+hg <- within(subset(hgTables,!grepl("hap",X.chrom)&!grepl("Un",X.chrom)&!grepl("random",X.chrom)&!grepl(";",geneName)&geneName!=""),
+{
+  chr <- sub("chr","",X.chrom)
+  start <- chromStart
+  end  <- chromEnd
+})[c("chr","start","end","geneName","UniProt")]
+subset(hg,grepl("Q14213",UniProt))
+subset(hg,grepl("Q29980",UniProt))
+subset(hg,grepl("P21217",UniProt))
+subset(hg,grepl("O43521",UniProt))
+# does not exist
+subset(hg,grepl("P16284",UniProt))
+hg <- rbind(hg,data.frame(chr="17", start=62396776, end=62407083, geneName="PECAM1", UniProt="P16284"))
 names(Olink_NGS)[2] <- "gene"
+subset(Olink_NGS,grepl("Q14213",UniProt))
+subset(Olink_NGS,grepl("Q29980",UniProt))
+subset(Olink_NGS,grepl("P21217",UniProt))
+subset(Olink_NGS,grepl("O43521",UniProt))
+Olink_NGS[with(Olink_NGS,UniProt=="Q14213_Q8NEV9"),"UniProt"] <- "Q14213"
+Olink_NGS[with(Olink_NGS,UniProt=="Q14213"),"gene"] <- "EBI3"
+Olink_NGS[with(Olink_NGS,UniProt=="Q29980_Q29983"),"UniProt"] <- "Q29980"
+Olink_NGS[with(Olink_NGS,UniProt=="Q29980"),"gene"] <- "MICB"
+Olink_NGS[with(Olink_NGS,UniProt=="P21217_Q11128"),"UniProt"] <- "P21217"
+Olink_NGS[with(Olink_NGS,UniProt=="P21217"),"gene"] <- "PUT3"
+Olink_NGS[with(Olink_NGS,UniProt=="O43521-2"),"UniProt"] <- "O43521"
+Olink_NGS[with(Olink_NGS,UniProt=="O43521"),"gene"] <- "BCL2L11"
 p1 <- subset(Olink_NGS,Panel=="NEUROLOGY")
 p2 <- subset(Olink_NGS,Panel=="CARDIOMETABOLIC")
 p3 <- subset(Olink_NGS,Panel=="ONCOLOGY")
@@ -42,59 +69,34 @@ VennDiagram::venn.diagram(x = p, category.names=cnames, filename='ngs.png', imag
 
 pval <- Sys.getenv("pval")
 f <- paste0("NGS.",pval)
-# Chrom Start End P prot MarkerName
-sentinels <- read.table(f,as.is=TRUE,header=TRUE)
-hits <- within(sentinels,
+sentinels <- within(read.table(f,as.is=TRUE,header=TRUE),
 {
+# Chrom Start End P prot MarkerName
   panel_uniprot <- strsplit(prot,"_")
   Panel <- unlist(lapply(panel_uniprot,"[[",1))
   UniProt <- unlist(lapply(panel_uniprot,"[[",2))
   UniProt <- unlist(lapply(strsplit(UniProt,"-"),"[[",1))
-  prot <- paste(Panel,UniProt,sep="_")
+  prot <- paste0(Panel,"_",UniProt)
   SNP <- MarkerName
   chrpos <- strsplit(MarkerName,":")
   chr <- unlist(lapply(chrpos,"[[",1))
   Chr <- sub("chr","",chr)
-  bp <- unlist(lapply(chrpos,"[[",2))
+  bp <- as.integer(unlist(lapply(chrpos,"[[",2)))
 })[ c("prot","Chr","bp","SNP","UniProt")]
+hits <- merge(sentinels,Olink_NGS,by="UniProt")
 
-options(width=200)
-
-hg <- within(subset(hgTables,!grepl("hap",X.chrom)&!grepl("Un",X.chrom)&!grepl("random",X.chrom)&!grepl(";",geneName)&geneName!=""),
-{
-  chr <- sub("chr","",X.chrom)
-  start <- chromStart
-  end  <- chromEnd
-  gene <- geneName
-})[c("chr","start","end","gene","UniProt")]
-ngs <- within(merge(Olink_NGS,hg,by.x=c("UniProt","gene"),by.y=c("UniProt","gene")),{prot <- paste0(Panel,"_",UniProt)})
-pcvt <- cis.vs.trans.classification(hits=hits, panel=ngs, id="UniProt")
-
-pcvt$table
-
-bioc()$table
-
-library(iBMQ)
-# UniProt
-pqtl <- hits[c("UniProt","SNP", "prot")]
+ngs <- within(merge(hg,Olink_NGS,by="UniProt"),{prot <- paste0(Panel,"_",UniProt)})
+pqtl <- hits[c("gene","SNP","prot")]
 snp <- hits[c("SNP","Chr","bp")]
-gene <- ngs[c("UniProt","chr","start","end")]
-UniProt.type <- eqtlClassifier(pqtl, snp, gene, 1000000)
-UniProt.table <- with(UniProt.type,table(UniProt,Type))
-
-# Gene
-all <- merge(ngs,hits,by="UniProt")
-pqtl <- all[c("gene","SNP","Panel")]
 gene <- ngs[c("gene","chr","start","end")]
 gene.type <- eqtlClassifier(pqtl, snp, gene, 1000000)
 gene.table <- with(gene.type,table(gene,Type))
 sink(paste(pval,"cis.vs.trans.out",sep="/"))
-UniProt.type
-UniProt.table
-cat("Total",sum(UniProt.table[,1]),sum(UniProt.table[,2]),sum(UniProt.table[,3]),"\n")
-sum(UniProt.table)
+row.names(gene.type) <- 1:nrow(gene.type)
 gene.type
 gene.table
 cat("Total",sum(gene.table[,1]),sum(gene.table[,2]),"\n")
 sum(gene.table)
 sink()
+cvt <- cis.vs.trans.classification(hits=hits[c("Chr","bp","gene","prot")], panel=ngs[c("chr","start","end","gene","prot")], id="prot")
+cvt
