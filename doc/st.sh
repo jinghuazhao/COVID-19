a9fbff5ec1428c6bc5fb64e872604b7334e1fd4b
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-08-29 10:36:31 +0100

    Documents and auxiliary files

--- doc/st.sh
+++ doc/st.sh
@@ -12,7 +12,7 @@ git add ACE2/2805-6_2.metal ACE2/select.sh ACE2/ukb-ACE2-annotate.sh ACE2/ace2_i
 git commit -m "SomaLogic"
 git add GEO/GEO.R GEO/GEO2R.R GEO/test.ini GEO/test.R GEO/test.sh GEO/README.md
 git commit -m "GEO test"
-git add HGI/INTERVAL.do HGI/INTERVAL.inc HGI/INTERVAL.rec HGI/INTERVAL.sh HGI/autosomes.sb HGI/glist-hg19.sb HGI/glist-hg19.sh HGI/select.sh
+git add HGI/INTERVAL.do HGI/INTERVAL.rec HGI/INTERVAL.sh HGI/autosomes.sb HGI/glist-hg19.sb HGI/glist-hg19.sh HGI/select.sh
 git add HGI/20200731.sh
 git commit -m "HGI analysis"
 git add NGS/README.* NGS/ngs.png NGS/ngs.do NGS/ngs.R NGS/ngs.sb NGS/ngs.sh NGS/lod.sh NGS/pQTLtools.R NGS/utils
