92755cd52111b9b6c0745212a84ea00981e28f6d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-08-31 09:36:46 +0100

    HGI analysis

--- HGI/INTERVAL.sh
+++ HGI/INTERVAL.sh
@@ -136,7 +136,7 @@ do
   Cx_V2_step1
 done
 
-export covlist=sex,PC_1,PC_2,PC_3,PC_4,PC_5,PC_6,PC_7,PC_8,PC_9,PC_10,PC_11,PC_12,PC_13,PC_14,PC_15,PC_16,PC_17,PC_18,PC_19,PC_20
+export covlist=age,age2,PC_1,PC_2,PC_3,PC_4,PC_5,PC_6,PC_7,PC_8,PC_9,PC_10,PC_11,PC_12,PC_13,PC_14,PC_15,PC_16,PC_17,PC_18,PC_19,PC_20
 for dir in ${d}-male-ANA_C1_V2 ${d}-male-ANA_C2_V2 ${d}-female-ANA_C1_V2 ${d}-female-ANA_C2_V2
 do
   export dir=${dir}
