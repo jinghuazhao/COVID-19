a782bd4f6d1e3d769844a305d11ad9ecdf0e19c9
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-09-30 10:26:51 +0100

    SCALLOP/WES-WGS meta-analysis

---
+++ WESWGS/rva.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/bash
+
+# Group file
+#Column no.	Column name	Description
+#1	CHR	Chromosome number (1-22)
+#2	POS	Physical base-pair position on the chromosome (in b38 coordinates)
+#3	REF	Reference allele
+#4	ALT	Alternate allele
+#5	AC	Allele count in genotypes for each ALT allele
+#6	AN	Total number of alleles in called genotypes
+
+# one group file per filtering/weighting scheme
+
+# score statistics and covariance matrices
+
+# By singularity container `burden`
+
+# inputs
+# - Filtered VCF file (see above section on Variant QC) OR GDS files if they already exist
+# - Phenotype files containing two columns: sample ID and INT-transformed, covariate-adjusted and renormalised residuals without header
+# - Genetic relatedness matrix (GRM) with sample IDs as row and column names
+
+# outputs
+# - A space-delimited file containing single variant scores
+# - A binary file containing between-variant covariance matrices
---
+++ WESWGS/spa.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+#Column no	Column name	Description
+#1 	SNP	rsID, or NA if unavailable
+#2 	CHR	Chromosome number
+#3 	POS	Physical base pair position on the chromosome (in b38 coordinates)
+#4 	N 	Number of non-missing observations
+#5 	EFF_ALLELE 	Allele whose effect is reported (beta estimates)
+#6	OTHER_ALLELE 	The other allele at the SNP
+#7 	EFF_ALLELE_FREQ	Allele frequency of EFF_ALLELE
+#8 	BETA	Effect size estimate, to at least 5 d.p.
+#9	SE	Standard error of the beta estimates, to at least 5 d.p.
+#10	P_LRT	P-value LRT
+#11	P_SCORE	P-value score
+
+# <olink_protein>_<cohort>_<date_of_analysis>_<analyst_initials>.txt.bgz
+# ACE2_pooled_MANOLIS_28102019_GP.txt.bgz
