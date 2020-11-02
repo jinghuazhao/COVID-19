d94cfc85138e0b21c30c3433b778b2b28b71de7d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-11-02 19:46:45 +0000

    SCALLOP/WES-WGS meta-analysis

--- WESWGS/rva.sh
+++ WESWGS/rva.sh
@@ -1,20 +1,5 @@
 #!/usr/bin/bash
 
-# Group file
-#Column no.	Column name	Description
-#1	CHR	Chromosome number (1-22)
-#2	POS	Physical base-pair position on the chromosome (in b38 coordinates)
-#3	REF	Reference allele
-#4	ALT	Alternate allele
-#5	AC	Allele count in genotypes for each ALT allele
-#6	AN	Total number of alleles in called genotypes
-
-# one group file per filtering/weighting scheme
-
-# score statistics and covariance matrices
-
-# By singularity container `burden`
-
 # inputs
 # - Filtered VCF file (see above section on Variant QC) OR GDS files if they already exist
 # - Phenotype files containing two columns: sample ID and INT-transformed, covariate-adjusted and renormalised residuals without header
@@ -25,3 +10,12 @@
 # - A binary file containing between-variant covariance matrices
 
 # Meta-analysis
+
+# Group file
+#Column no.	Column name	Description
+#1	CHR	Chromosome number (1-22)
+#2	POS	Physical base-pair position on the chromosome (in b38 coordinates)
+#3	REF	Reference allele
+#4	ALT	Alternate allele
+#5	AC	Allele count in genotypes for each ALT allele
+#6	AN	Total number of alleles in called genotypes
--- WESWGS/spa.sh
+++ WESWGS/spa.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/bash
 
+cd burden_testing
+export COHORT_NAME=INTERVAL
+for panel in cv2 cvd3
+do
+  singularity exec -B ${pwd} burden.1.6.5 step1 ${COHORT_NAME} work/${panel}-wes.vcf.gz
+  for chr in chr{1.22} chrX chrY
+  do
+    export chr=chr22
+    export WGS=~/COVID-19/WESWGS/work/${panel}-wgs-${chr}.vcf.gz
+    singularity exec -B $(pwd) burden.1.6.5 step1 $COHORT_NAME ${WGS}
+  done
+done
+
+# SLURM
+# sbatch --mem 2G -e step1.e -o step1.o --wrap "singularity exec -B $(pwd) burden.1.6.5 step1 $COHORT_NAME $VCF_PATH"
+
+# <olink_protein>_<cohort>_<date_of_analysis>_<analyst_initials>.txt.bgz
+# ACE2_INTERVAL_02112020_JHZ.txt.bgz
+
 #Column no	Column name	Description
 #1 	SNP	rsID, or NA if unavailable
 #2 	CHR	Chromosome number
@@ -12,15 +31,3 @@
 #9	SE	Standard error of the beta estimates, to at least 5 d.p.
 #10	P_LRT	P-value LRT
 #11	P_SCORE	P-value score
-
-# <olink_protein>_<cohort>_<date_of_analysis>_<analyst_initials>.txt.bgz
-# ACE2_pooled_MANOLIS_28102019_GP.txt.bgz
-
-cd burden_testing
-export COHORT_NAME=INTERVAL
-export chr=22
-export VCF_PATH=~/COVID-19/WESWGS/wgs/chr${chr}/chr${chr}.intervalwgs_v2_GT_only.vcf.bgz
-singularity exec -B $(pwd) burden.1.6.5 step1 $COHORT_NAME $VCF_PATH
-
-# slurm
-# sbatch --mem 2G -e step1.e -o step1.o --wrap "singularity exec -B $(pwd) burden.1.6.5 step1 $COHORT_NAME $VCF_PATH"
