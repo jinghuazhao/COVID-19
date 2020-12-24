2bd2f3acb48dd1fcdde00e047bc6457fe7f6d05d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-12-24 09:05:54 +0000

    HGI analysis

--- HGI/INTERVAL.sh
+++ HGI/INTERVAL.sh
@@ -136,18 +136,18 @@ function Cx_V2_step1_X()
 }
 
 export d=20201201
-# export d=20200731
-# export covlist=sex,age,age2,sexage,PC_1,PC_2,PC_3,PC_4,PC_5,PC_6,PC_7,PC_8,PC_9,PC_10,PC_11,PC_12,PC_13,PC_14,PC_15,PC_16,PC_17,PC_18,PC_19,PC_20
+export covlist=sex,age,age2,sexage,PC_1,PC_2,PC_3,PC_4,PC_5,PC_6,PC_7,PC_8,PC_9,PC_10,PC_11,PC_12,PC_13,PC_14,PC_15,PC_16,PC_17,PC_18,PC_19,PC_20
 # for dir in ${d}-ANA_C1_V2 ${d}-ANA_C2_V2
-# do
-#   export dir=${dir}
-#   Cx_V2_step1
-#   Cx_V2_step1_X
-# done
+for dir in ${d}-ANA_C2_V2
+do
+  export dir=${dir}
+  Cx_V2_step1
+  Cx_V2_step1_X
+done
 
 export covlist=age,age2,PC_1,PC_2,PC_3,PC_4,PC_5,PC_6,PC_7,PC_8,PC_9,PC_10,PC_11,PC_12,PC_13,PC_14,PC_15,PC_16,PC_17,PC_18,PC_19,PC_20
 # for dir in ${d}-male-ANA_C1_V2 ${d}-male-ANA_C2_V2 ${d}-female-ANA_C1_V2 ${d}-female-ANA_C2_V2
-for dir in ${d}-ANA_C2_V2 ${d}-male-ANA_C2_V2 ${d}-female-ANA_C2_V2
+for dir in ${d}-male-ANA_C2_V2 ${d}-female-ANA_C2_V2
 do
   export dir=${dir}
   Cx_V2_step1
