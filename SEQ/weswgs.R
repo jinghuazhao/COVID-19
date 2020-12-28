6d823f4f92836cd89e3a16049cc1232dff284e40
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2020-12-28 12:10:00 +0000

    SCALLOP-seq meta-analysis

--- SEQ/weswgs.R
+++ SEQ/weswgs.R
@@ -19,6 +19,8 @@ cvd3 <- olink("cvd3")
 inf1 <- olink("inf1")
 neu <- olink("neu")
 
+options(width=200)
+require(plyr)
 library(dplyr)
 y <- cvd2[c(1:92,104)] %>%
      full_join(cvd3[c(1:92,104)],by="Aliquot_Id") %>%
@@ -60,9 +62,9 @@ y_wgs <- id_wgs %>% select(-c(phase,wes_id)) %>%
 rownames(y_wgs) <- y_wgs[["wgs_id"]]
 
 library(gap)
-normalize <- function(d,id)
+normalize_sapply <- function(d,id)
 {
-  normfun <- function(col,verbose=TRUE)
+  normfun <- function(col,verbose=FALSE)
   {
     if (verbose) cat(names(d[col]),col,"\n")
     y <- invnormal(d[[col]])
@@ -77,35 +79,40 @@ normalize <- function(d,id)
   rownames(z) <- d[[id]]
   as.data.frame(z)
 }
-y_wes_n <- normalize(y_wes,"wes_id")
-a <- y_wes_n["neu_KYNU___Q16719"]
-l <- lm(invnormal(neu_KYNU___Q16719)~sexPulse+agePulse,data=y_wes)
-r <- y_wes["neu_KYNU___Q16719"]-predict(l,na.action=na.pass)
+y_wes_sapply <- normalize_sapply(y_wes,"wes_id")
+y_wgs_sapply <- normalize_sapply(y_wgs,"wgs_id")
+
+require(doMC)
+doMC::registerDoMC(cores = 14)
+normalize_adply <- function(d)
+{
+  proteins <- names(d)[grep("cvd2|cvd3|inf1|neu",names(d))]
+  sexage <- d[grep("sex|age",names(d))]
+  z <- adply(d[proteins], 2, function(x)
+       {
+          y <- invnormal(x)
+          l <- lm(y~sexPulse+agePulse,data=sexage)
+          r <- y-predict(l,na.action=na.pass)
+          invnormal(r)
+       }, .progress = "none", .parallel = TRUE)
+  tnames <- z[,1]
+  z <- t(z[,-1])
+  colnames(z) <- tnames
+  rownames(z) <- rownames(d)
+  as.data.frame(z)
+}
+y_wes_adply <- normalize_adply(y_wes)
+y_wgs_adply <- normalize_adply(y_wgs)
+
+identical(y_wgs_sapply,y_wgs_adply)
+identical(y_wgs_sapply,y_wgs_adply)
+
+a <- y_wes_sapply["cvd2_BMP.6"]
+l <- lm(invnormal(cvd2_BMP.6)~sexPulse+agePulse,data=y_wes)
+r <- y_wes["cvd2_BMP.6"]-predict(l,na.action=na.pass)
 b <- invnormal(r)
 plot(cbind(a,b))
-y_wgs_n <- normalize(y_wgs,"wgs_id")
-
-normalize <- function(d)
-{
-  require(plyr)
-  require(doMC)
-  doMC::registerDoMC(cores = 14)
-  require(gdata)
-  require(data.table)
-  result <- rbindlist(alply(names(d), 1, function(obs) {
-          tryCatch({
-                  y <- d[[obs]]
-                  y <- invnormal(d[[col]])
-                  l <- lm(y~sexPulse+agePulse,data=sexage)
-                  r <- y-predict(l,na.action=na.pass)
-                  return(invnormal(r))
-          }, error = function(e) {
-                  return(-1)
-          })}, .progress = "none", .parallel = TRUE))
-
-  result <- as.data.frame(result)
-}
-y_wes_n[proteins] <- normalize(y_wes[proteins])
+head(cbind(a,b))
 
 ## overlaps
 
