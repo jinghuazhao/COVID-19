f255d921e0c2dd3eabe2c973f49702553cc8ab9c
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-03-24 14:31:02 +0000

    HGI analysis

--- HGI/pca_project.sh
+++ HGI/pca_project.sh
@@ -7,20 +7,16 @@ export autosomes=~/rds/post_qc_data/interval/imputed/uk10k_1000g_b37/
 export ref=~/rds/post_qc_data/interval/reference_files/genetic/reference_files_genotyped_imputed/
 # HGI working directory
 export prefix=~/rds/rds-asb38-ceu-restricted/projects/covid/HGI
-export dir=20201201-male-ANA_C2_V2
+export dir=20201201-ANA_C2_V2
 
 export PCA_projection=pca_projection
 export PCA_loadings=hgdp_tgp_pca_covid19hgi_snps_loadings.GRCh37.plink.tsv
 export PCA_af=hgdp_tgp_pca_covid19hgi_snps_loadings.GRCh37.plink.afreq
 export sscore=hgdp_tgp_pca_covid19hgi_snps_scores.txt.gz
 
+# INTERVAL.bgen via SNP 150
 function snpid_rsid()
 {
-  cut -f1 ${PCA_projection}/${PCA_loadings} | tail -n +2 > variants.extract
-  (
-    cat variants.extract
-    awk '{split($1,a,":");print "chr"a[1]":"a[2]"_"a[4]"_"a[3]}' variants.extract
-  ) > variants.extract2
   awk '{split($1,a,":");if(a[3]<a[4]) print "chr"a[1]":"a[2]"_"a[3]"_"a[4];else print "chr"a[1]":"a[2]"_"a[4]"_"a[3]}' variants.extract > snpid.extract
   join <(sort -k1,1 snpid.extract) <(gunzip -c ${HPC_WORK}/SUMSTATS/snp150.snpid_rsid.gz) > snpid-rsid.extract
   cut -d' ' -f2 snpid-rsid.extract > rsid.extract
@@ -32,7 +28,6 @@ function bgen_only_as_documented()
   parallel -C' ' --env prefix --env dir ' bgenix -g ${prefix}/${dir}/output/INTERVAL-{}.bgen -incl-rsids rsid.extract > ${prefix}/work/INTERVAL-{}.bgen'
   cat-bgen -g $(echo ${prefix}/work/INTERVAL-{1..22}.bgen) -og ${prefix}/work/INTERVAL.bgen -clobber
   bgenix -g ${prefix}/work/INTERVAL.bgen -index
-  qctool -g ${prefix}/work/INTERVAL.bgen -og ${prefix}/work/INTERVAL.vcf.gz
 }
 
 function bgen_sample()
@@ -46,13 +41,14 @@ function bgen_sample()
   '
 }
 
+# INTERVAL.bgen via SNPid
 function map()
 {
   seq 22 | \
   parallel --env prefix -C' ' '
     (
       echo alternate_ids rsid chromosome position allele1 allele2 rsid SNPID chromosome position allele1 allele2
-      bgenix -g ${prefix}/work/INTERVAL-{}.bgen -list 2>&1 | \
+      bgenix -g ${prefix}/${dir}/output/INTERVAL-{}.bgen -list 2>&1 | \
       sed "1,9d" | \
       awk "
       {
@@ -71,16 +67,15 @@ function map()
 
 function snpid()
 {
-  seq 22 | \
-  parallel --env prefix -C' ' '
-    qctool -g ${prefix}/work/INTERVAL-{}.bgen -s ${prefix}/work/INTERVAL.samples \
-           -incl-rsids ${prefix}/work/INTERVAL-{}.nodup -map-id-data ${prefix}/work/INTERVAL-{}.map \
-           -bgen-bits 8 \
-           -og ${prefix}/work/snpid-{}.bgen -os ${prefix}/work/snpid-{}.samples
-    bgenix -g ${prefix}/work/snpid-{}.bgen -index -clobber
-  '
+  sbatch --wait ${prefix}/pca_project.sb
   cat-bgen -g $(echo ${prefix}/work/snpid-{1..22}.bgen) -og ${prefix}/work/snpid.bgen -clobber
   bgenix -g ${prefix}/work/snpid.bgen -index
+  (
+    cat variants.extract
+    awk '{split($1,a,":");print "chr"a[1]":"a[2]"_"a[4]"_"a[3]}' variants.extract
+  ) > variants.extract2
+  bgenix -g ${prefix}/work/snpid.bgen -incl-rsids variants.extract2 ${prefix}/work/INTERAL.bgen
+  bgenix -g ${prefix}/work/INTERVAL.bgen -index -clobber
 }
 
 function install_R_packages()
@@ -88,16 +83,16 @@ function install_R_packages()
   Rscript -e "install.packages(c("data.table", "hexbin", "optparse", "patchwork", "R.utils", "tidyverse"))"
 }
 
-snpid_rsid;bgen_sample;map;snpid
+cut -f1 ${PCA_projection}/${PCA_loadings} | tail -n +2 > variants.extract
+# snpid_rsid;bgen_sample;
+map;snpid
 
 module load plink/2.00-alpha
-plink2 --bgen ${prefix}/work/snpid.bgen ref-first --make-pfile --out ${prefix}/work/snpid
+plink2 --bgen ${prefix}/work/INTERVAL.bgen ref-first --make-pfile --out ${prefix}/work/INTERVAL
 
-# The "chr" prefix in the provided file is unnecessary
-sed 's/chr//g' ${PCA_projection}/${PCA_af} > ${prefix}/work/PCA_af.tsv
-plink2 --pfile ${prefix}/work/snpid \
+plink2 --pfile ${prefix}/work/INTERVAL \
        --score ${PCA_projection}/${PCA_loadings} center cols=-scoreavgs,+scoresums list-variants header-read \
-       --score-col-nums 3-22 --read-freq ${prefix}/work/PCA_af.tsv --out ${prefix}/work/init
+       --score-col-nums 3-22 --read-freq ${PCA_projection}/${PCA_af} --out ${prefix}/work/init
 # The score file does not have FID (=0)
 awk '
 {
@@ -134,15 +129,3 @@ Rscript ${PCA_projection}/plot_projected_pc.R \
   --study INTERVAL \
   --out ${prefix}/work/{}-INTERVAL
 ' ::: AFR AMR EAS EUR MID SAS
-
-Rscript ${PCA_projection}/plot_projected_pc.R \
-  --sscore ${prefix}/work/snpid.sscore \
-  --phenotype-file ${prefix}/work/snpid.pheno \
-  --phenotype-col SARS_CoV \
-  --covariate-file ${prefix}/work/snpid.covars \
-  --pc-prefix PC \
-  --pc-num 20 \
-  --ancestry "AFR AMR EAS EUR MID SAS" \
-  --study INTERVAL \
-  --out ${prefix}/work/All-INTERVAL
-
