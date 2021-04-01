326435b6895d02d959fba8779f9d451b944d1528
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-04-01 09:50:50 +0100

    Documents and auxiliary files

--- doc/st.sh
+++ doc/st.sh
@@ -14,7 +14,7 @@ git add GEO/GEO.R GEO/GEO2R.R GEO/test.ini GEO/test.R GEO/test.sh GEO/README.md
 git commit -m "GEO test"
 git add HGDP/README.md
 git add HGDP/EPIC-Omics.sh HGDP/HGI.* HGDP/refdata.* HGDP/MDSplot.R HGDP/keep.* 
-git add  HGDP/nonEUR.sh HGDP/nonEUR.R HGDP/nonEUR.sb
+git add  HGDP/nonEUR.sh HGDP/nonEUR.R HGDP/nonEUR.sb HGDP/nonEUR.C1-C2.png
 git commit -m "1000G mappings"
 git add HGI/README.md HGI/INTERVAL.do HGI/INTERVAL.rec HGI/INTERVAL.sh HGI/autosomes.sb HGI/X.sb HGI/X-female.sb
 git add HGI/glist-hg19.sb HGI/glist-hg19.sh HGI/L10RB_IFNAR2_variants.sh
