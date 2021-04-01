5a66e7daf7bd4e35e081f78364aed4f71f5ba4db
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-04-01 10:03:22 +0100

    1000G mappings

--- HGDP/HGI.sh
+++ HGDP/HGI.sh
@@ -27,4 +27,4 @@ plink --bfile mydata.refdata.QCed --read-genome mydata.refdata.QCed.Z.genome.gz 
 
 # STEP5: MDS-PLOT
 
-Rscript --slave mydata.refdata.QCed.MDS.mds refdata.fam MyOutput < MDSplot.R
+Rscript MDSplot.R mydata.refdata.QCed.MDS.mds refdata.fam MyOutput
