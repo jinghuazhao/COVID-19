b3820957df234ec9f4d83c2df7b832dc5405e88d
jinghuazhao@github.com <jinghuazhao@hotmail.com>
2021-04-01 08:16:18 +0100

    Documents and auxiliary files

--- doc/st.sh
+++ doc/st.sh
@@ -12,7 +12,7 @@ git add ACE2/2805-6_2.metal ACE2/select.sh ACE2/ukb-ACE2-annotate.sh ACE2/ace2_i
 git commit -m "SomaLogic"
 git add GEO/GEO.R GEO/GEO2R.R GEO/test.ini GEO/test.R GEO/test.sh GEO/README.md
 git commit -m "GEO test"
-git add HGDP/EPIC-Omics.sh HGDP/HGI.sh HGDP/refdata.* HGDP/MDSplot.R HGDP/keep.* HGDP/README.md
+git add HGDP/EPIC-Omics.sh HGDP/HGI.* HGDP/refdata.* HGDP/MDSplot.R HGDP/keep.* HGDP/README.md
 git commit -m "1000G mappings"
 git add HGI/README.md HGI/INTERVAL.do HGI/INTERVAL.rec HGI/INTERVAL.sh HGI/autosomes.sb HGI/X.sb HGI/X-female.sb
 git add HGI/glist-hg19.sb HGI/glist-hg19.sh HGI/L10RB_IFNAR2_variants.sh
